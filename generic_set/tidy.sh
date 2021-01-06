497315a1027cae20a7d46d52e479e8b2cd2cac27
Kumamoto <onioni9999nagare@gmail.com>
2021-01-06 22:01:25 +0900

    make tidy.sh for making dir-arrange easily

---
+++ generic_set/tidy.sh
@@ -0,0 +1,13 @@
+# 全ての値を出力する場合は、インデックスに「@」を指定するんや
+# -a: and  -o: or
+abc_array=("a" "b" "c" "d" "e" "f")
+for i in "${abc_array[@]}"
+do
+	if test ! -e ${i} -a -e ${i}*.py ; then
+		echo "${i}"
+		mkdir "${i}"
+		mv ${i}*.py ${i}/
+	elif test -e ${i}*.py ; then
+		mv ${i}*.py ${i}/
+	fi
+done
