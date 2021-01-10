150f847f5749d650d8e65b9ad1e1d5e638ca7ca6
Kumamoto <onioni9999nagare@gmail.com>
2021-01-10 12:43:51 +0900

    add comment for abc171-c

--- abc_contest/abc171/c/c3.py
+++ abc_contest/abc171/c/c3.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     while True:
         n -= 1 #for 0 index
         order = n % 26 # 26 ** 1 * n + 26 ** 2 * m + ... 26 ** 10 * p + order(0~25)
-        ans += chr(order + ord("a"))
+        ans += chr(order + ord("a")) # (1)ansを内部で定義したのに
         if n < 26:
             break
         n //= 26
-    print(ans[::-1])
+    print(ans[::-1]) # (2)スコープの外で参照している、こういうコードは望ましくない
 
     """
     input_str = snput()
