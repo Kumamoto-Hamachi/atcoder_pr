df5b04dcdc0b50963f8253e19789f5b4a7781061
Kumamoto <onioni9999nagare@gmail.com>
2021-01-12 23:26:53 +0900

    resolve abc158-a

--- abc_contest/abc158/tidy.sh
+++ abc_contest/abc158/tidy.sh
@@ -3,11 +3,9 @@
 abc_array=("a" "b" "c" "d" "e" "f")
 for i in "${abc_array[@]}"
 do
-	if test ! -e ${i} -a -e ${i}*.py ; then
-		echo "${i}"
-		mkdir "${i}"
-	fi
 	if test -e ${i}*.py ; then
+		echo "${i}"
+		mkdir -p "${i}"
 		mv ${i}*.py ${i}/
 	fi
 done
