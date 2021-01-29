a17b925a21fa92289129bc68bd05f9f225b558f8
Kumamoto <onioni9999nagare@gmail.com>
2021-01-29 18:09:27 +0900

    solve abc154-b

---
+++ abc_contest/abc154/b/b.py
@@ -0,0 +1,16 @@
+import sys
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    S_len = len(sreadline())
+    print("x" * S_len)
+
+    """
+    input_str = snput()
+    input_num = int(snput())
+    some_map = m_snput()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    """
