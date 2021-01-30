9d79464fabe09284a2db5ef41e5c4ab207351398
Kumamoto <onioni9999nagare@gmail.com>
2021-01-30 20:50:53 +0900

    renew standard input functions

--- generic_set/main.py
+++ generic_set/main.py
@@ -2,14 +2,14 @@ from pprint import pprint as pp
 from collections import defaultdict
 import sys
 sys.setrecursionlimit(10 ** 7)
-# readlines = sys.stdin.buffer.readlines
-# map_readlines = lambda: map(int, readlines())
-# readline = sys.stdin.buffer.readline
-# map_readline = lambda: map(int, readline().split())
-# sreadline = lambda: readline().decode("utf-8")
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
 # read
-snput = sys.stdin.buffer.readline
-m_snput = lambda: map(int, snput().split())
+# snput = sys.stdin.buffer.readline
+# m_snput = lambda: map(int, snput().split())
 
 if __name__ == "__main__":
     """
