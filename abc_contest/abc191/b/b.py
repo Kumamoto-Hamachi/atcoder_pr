60120c346b416fc709b8688afdbaa9dd60f7c9ec
Kumamoto <onioni9999nagare@gmail.com>
2021-02-06 23:23:51 +0900

    solve abc191-b

---
+++ abc_contest/abc191/b/b.py
@@ -0,0 +1,24 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    N, X = map_readline()
+    a_l = list(map_readline())
+    n_l = [str(i) for i in a_l if i != X]
+    a = " ".join(n_l)
+    print(a)
+
+    """
+    input_str = snput()
+    input_num = int(snput())
+    some_map = m_snput()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    """
