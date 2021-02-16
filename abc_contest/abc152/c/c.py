d2e7d6ea95d273028565b68beb070e7783604082
Kumamoto <onioni9999nagare@gmail.com>
2021-02-17 00:11:57 +0900

    resolve c

--- abc_contest/abc152/c/c.py
+++ abc_contest/abc152/c/c.py
@@ -2,17 +2,19 @@ import sys
 import math
 readline = sys.stdin.buffer.readline
 map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
 
 if __name__ == "__main__":
     N = int(readline())
     p_l = list(map_readline())
-    cnt = 0
     min_num = math.inf
-    for i in range(N):
-        min_num = min(min_num, p_l[i])
-        if min_num == p_l[i]:
+    cnt = 0
+    for p in p_l:
+        min_num = min(min_num, p)
+        if min_num == p:
             cnt += 1
     print(cnt)
+
 
     """
     input_str = readline()
