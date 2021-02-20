43c39bd5b7cb21dd3c0b1921b5de3666892db7be
Kumamoto <onioni9999nagare@gmail.com>
2021-02-21 04:04:17 +0900

    solve abc192-a

---
+++ abc_contest/abc192/a/a.py
@@ -0,0 +1,25 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+import math
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    N = int(readline())
+    if N % 100 == 0:
+        print(100)
+    else:
+        tmp = N % 100
+        print(100 - tmp)
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    """
