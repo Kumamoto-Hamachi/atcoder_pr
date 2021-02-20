3bfe14bd35878d4a6e8725aee0d48e305faa5866
Kumamoto <onioni9999nagare@gmail.com>
2021-02-21 04:04:30 +0900

    solve abc192-b

---
+++ abc_contest/abc192/b/b.py
@@ -0,0 +1,26 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    S = sreadline()
+    num = 1
+    for s in S:
+        if (s.isupper() + num) % 2 == 0:
+            print("No")
+            sys.exit()
+        num = 0 if num == 1 else 1
+    print("Yes")
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    """
