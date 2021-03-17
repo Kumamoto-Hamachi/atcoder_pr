f08bbaefdf822e6f5630d5def888d1a3d986f2c2
Kumamoto <onioni9999nagare@gmail.com>
2021-03-17 10:59:18 +0900

    solve abc182-b and mv file

---
+++ abc_contest/abc182/b/b4.py
@@ -0,0 +1,28 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    n = int(readline())
+    a_l = list(map_readline())
+    limit = max(a_l)
+    ans, max_cnt = 0, 0
+    for i in range(2, limit+1):
+        cnt = len([a for a in a_l if a % i == 0])
+        ans, max_cnt = (i, cnt) if cnt >= max_cnt else (ans, max_cnt)
+    print(ans)
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    sreadlines = [sb.decode("utf-8").rstrip() for sb in readlines()]
+    """
+
