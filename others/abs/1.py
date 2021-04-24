acf63eae20894374e5b45c97eda8ef867d2c21a7
Kumamoto <onioni9999nagare@gmail.com>
2021-04-25 01:11:31 +0900

    PracticeA Welcome to Atcoder

---
+++ others/abs/1.py
@@ -0,0 +1,23 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    a = int(readline())
+    b, c = map_readline()
+    s = sreadline()
+    print(str(a + b + c), s)
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    sreadlines = [sb.decode("utf-8").rstrip() for sb in readlines()]
+    """
