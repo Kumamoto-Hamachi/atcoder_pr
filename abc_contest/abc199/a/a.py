b6082b9af04e707dde87bfbaff63c094cabc4a1c
Kumamoto <onioni9999nagare@gmail.com>
2021-04-24 23:03:34 +0900

    solve abc199-a

---
+++ abc_contest/abc199/a/a.py
@@ -0,0 +1,24 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    a, b, c = map_readline()
+    if a ** 2 + b ** 2 < c ** 2:
+        print("Yes")
+    else:
+        print("No")
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    sreadlines = [sb.decode("utf-8").rstrip() for sb in readlines()]
+    """
