b1d095dc99b649ddd7192f1880e8eb785fa56950
Kumamoto <onioni9999nagare@gmail.com>
2021-05-01 23:10:00 +0900

    solve abczone-a

---
+++ abc_contest/abczone/a/a.py
@@ -0,0 +1,22 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+
+if __name__ == "__main__":
+    S = sreadline()
+    cnt = S.count('ZONe')
+    print(cnt)
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    sreadlines = [sb.decode("utf-8").rstrip() for sb in readlines()]
+    """
