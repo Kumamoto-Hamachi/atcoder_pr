b91f41422eb1d5b09c4a150996104330767c8eca
Kumamoto <onioni9999nagare@gmail.com>
2021-06-13 22:49:51 +0900

    add/a

---
+++ abc_contest/abc205/a/a.py
@@ -0,0 +1,23 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readlines = sys.stdin.buffer.readlines
+map_readlines = lambda: map(int, readlines())
+readline = sys.stdin.buffer.readline
+map_readline = lambda: map(int, readline().split())
+sreadline = lambda: readline().decode("utf-8").rstrip()
+sreadlines = lambda: [s.decode("utf-8").rstrip() for s in readlines()] # return strs per line list
+
+if __name__ == "__main__":
+    a, b = map_readline()
+    cal = (b * a) / 100
+    print(cal)
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    low_english_literature = " ".join(sreadlines())
+    """
