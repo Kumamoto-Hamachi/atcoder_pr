bd98bd897c10d3548c966fc15266e576c63a3a16
Kumamoto <onioni9999nagare@gmail.com>
2021-08-15 01:25:20 +0900

    add/best questions

---
+++ others/best_questions_for_middle/full_search/full_enumeration/main.py
@@ -0,0 +1,22 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+def map_readline(): return map(int, readline().split())
+def map_readlines(): return map(int, readlines())
+def sreadline(): return readline().decode("utf-8").rstrip()
+# return strs per line list
+def sreadlines(): return [s.decode("utf-8").rstrip() for s in readlines()]
+
+
+if __name__ == "__main__":
+    """
+    input_str = readline()
+    input_num = int(readline())
+    some_map = map_readline()
+    data = sreadline().split(" ")
+    data = list(sreadline())
+    low_english_literature = " ".join(sreadlines())
+    """
