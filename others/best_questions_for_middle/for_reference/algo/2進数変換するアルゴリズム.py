3c1b258728732236c76a0103c1fc2a123be124a8
Kumamoto <onioni9999nagare@gmail.com>
2021-08-21 01:40:56 +0900

    add/algo.2進数変換するアルゴリズム

---
+++ others/best_questions_for_middle/for_reference/algo/2進数変換するアルゴリズム.py
@@ -0,0 +1,15 @@
+def convert_10_to_2(num):
+    converted_num = ""
+    while True:
+        rest = num % 2
+        converted_num = str(rest) + converted_num
+        num //= 2
+        if rest == 0:
+            converted_num = str(num) + converted_num
+            return int(converted_num)
+
+
+if __name__ == "__main__":
+    n = 5
+    num = convert_10_to_2(n)
+    print("num", num)  # debug
