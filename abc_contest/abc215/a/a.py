663ed617e34cf2f78c061da4cc739f83d852c0bf
Kumamoto <onioni9999nagare@gmail.com>
2021-08-21 22:50:02 +0900

    add/a

---
+++ abc_contest/abc215/a/a.py
@@ -0,0 +1,20 @@
+from pprint import pprint as pp
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10 ** 7)
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+def map_readline(): return map(int, readline().split())
+def map_readlines(): return map(int, readlines())
+def sreadline(): return readline().decode("utf-8").rstrip()
+# return strs per line list
+def sreadlines(): return [s.decode("utf-8").rstrip() for s in readlines()]
+
+
+if __name__ == "__main__":
+
+    words = sreadline()
+    if words == "Hello,World!":
+        print("AC")
+    else:
+        print("WA")
