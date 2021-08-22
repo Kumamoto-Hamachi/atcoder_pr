4c460d644ce361d791d1a863348c7cd775ba1816
Kumamoto <onioni9999nagare@gmail.com>
2021-08-23 01:37:13 +0900

    add/abc147_c

---
+++ others/best_questions_for_middle/for_reference/abc147_c.py
@@ -0,0 +1,90 @@
+import sys
+sys.setrecursionlimit(10 ** 7)
+readline = sys.stdin.buffer.readline
+def map_readline(): return map(int, readline().split())
+
+
+def make_person_tesimony(n):
+    persons = [None] * n
+    for i in range(n):
+        speak_num = int(readline())
+        speaks = [None] * speak_num
+        for j in range(speak_num):
+            speaks[j] = list(map_readline())
+        persons[i] = speaks
+    return persons
+
+
+def is_valid_pattern(pattern, n, persons):
+    bin_pattern = make_bit_pattern(pattern, n)
+    for i in range(n-1, -1, -1):
+        if (pattern >> i) & 1:
+            if not is_all_speack_reliable(persons[n-1-i], bin_pattern): # TODO
+                return False
+    return True
+
+
+def make_bit_pattern(pattern, n):
+    bin_pattern = bin(pattern)[2:]
+    bin_pattern = (n - len(bin_pattern)) * "0" + bin_pattern
+    return bin_pattern
+
+
+def is_all_speack_reliable(speaks, bin_pattern):
+    #print("bin_pattern", bin_pattern)  # debug
+    for speak in speaks:
+        person_idx, state = speak
+        #print("speak", speak)  # debug
+        if int(bin_pattern[person_idx-1]) != state:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    n = int(readline())
+    persons = make_person_tesimony(n)
+
+    patterns = 2 ** n
+    max_cnt = 0
+    for pattern in range(patterns):
+        if is_valid_pattern(pattern, n, persons):
+            bin_pattern = make_bit_pattern(pattern, n)
+            max_cnt = max(max_cnt, bin_pattern.count("1"))
+    print(max_cnt)
+
+""" 深さ優先探索と二重配列
+import sys
+readline = sys.stdin.buffer.readline
+
+if __name__ == "__main__":
+    n = int(readline())
+    a = [0] * n
+    x = [[0] * n for i in range(n)]
+    y = [[0] * n for i in range(n)]
+     
+    for i in range(n):
+        a[i] = int(input())
+        for j in range(a[i]):
+            x[i][j], y[i][j] = map(int, input().split())
+            x[i][j] -= 1
+     
+     
+    def check(honest):
+        for i in range(n):
+            if not honest[i]:continue
+            for j in range(a[i]):
+                if y[i][j] == 1 and not honest[x[i][j]]:return False
+                if y[i][j] == 0 and honest[x[i][j]]:return False
+        return True
+            
+    def dfs(honest):
+        if len(honest) == n:
+            if check(honest):
+                return sum(honest)
+            else:
+                return 0
+        return max(dfs(honest + [True]),
+                   dfs(honest + [False]))
+        
+    print(dfs([]))
+"""
