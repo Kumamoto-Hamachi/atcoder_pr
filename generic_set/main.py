572159aa4edf20eab01dc0d82da8f2e206d73af8
Kumamoto <onioni9999nagare@gmail.com>
2021-08-28 23:23:09 +0900

    update/add math

--- generic_set/main.py
+++ generic_set/main.py
@@ -2,6 +2,7 @@ from pprint import pprint as pp
 from collections import defaultdict
 from itertools import permutations
 import sys
+import math
 sys.setrecursionlimit(10 ** 7)
 readline = sys.stdin.buffer.readline
 readlines = sys.stdin.buffer.readlines
