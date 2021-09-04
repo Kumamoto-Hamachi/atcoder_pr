815ee3513fd7c561025d8ccba64a6eb9010db4c1
Kumamoto <onioni9999nagare@gmail.com>
2021-09-04 23:30:47 +0900

    add/a

---
+++ abc_contest/abc217/a/a.py
@@ -0,0 +1,22 @@
+from pprint import pprint as pp
+from collections import defaultdict
+from itertools import permutations
+import sys
+import math
+sys.setrecursionlimit(10 ** 7)
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+def map_readline(): return map(int, readline().split())
+def map_readlines(): return map(int, readlines())
+def list_readline(): return list(map_readline())
+def sreadline(): return readline().decode("utf-8").rstrip()
+# return strs per line list
+def sreadlines(): return [s.decode("utf-8").rstrip() for s in readlines()]
+
+
+if __name__ == "__main__":
+    s, t = sreadline().split(" ")
+    if s < t:
+        print("Yes")
+    else:
+        print("No")
